8df998f5c9491c42dd176a456133132f43b007e2
Nick Kazban <79278683+NickKazban@users.noreply.github.com>
2021-05-26 13:34:06 +0300

    add files

---
+++ app.js
@@ -0,0 +1,38 @@
+$(function () {
+    /*Fixed HEADER
+    ==================*/
+    
+    let header = $("#jsHeader");
+    let intro = $("#jsIntro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll load resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else { 
+            header.removeClass("fixed");
+        }
+    });
+
+        /*Smooth scroll
+    ===========================*/
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset('top');
+
+        console.log(elementOffset);
+    });
+    
+});
+
+
+
+
+
